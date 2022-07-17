a0fcde3f8b2f108fe66c3fdf6af6039dae2a5b78
Ankit Shaji <ankitshaji@gmail.com>
2022-07-18 07:19:53 +1200

    setting rand backgrnd color to body object element

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-//Element object
+//Element object = document object.method(css element selector)
 const button = document.querySelector("button");
 //ElementObject.method(argument1,argument2)
 //argument1= stringEvent,argument2=anontmous callback function expression
 //addEventListner-when stringevent happens on target element Object
 //addEventLister executes the function expression
 button.addEventListener("click", function () {
-  console.log("test");
+  //color rgb range 255
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+  //body element object - shortcut
+  //bodyobject.style is CSSStyleDeclaration Object
+  //CSSStyleDeclarationObject.property
+  document.body.style.backgroundColor = `rgb(${r},${g},${b})`; //string template literal
 });
